2f67298e6cbf9bfaf16a6eb8de81cd13453995b2
KamalShamsiyev <ksmsiyev@gmail.com>
2022-09-24 10:32:37 +0400

    finished

---
+++ Kamal.js
@@ -0,0 +1,14 @@
+function clickEvent(){
+    document.getElementById("demo").style.background = "blue"
+    document.getElementById("demo").innerHTML = "Düzgündür!"
+}
+
+function hoverEvent(){
+    document.getElementById("atom").style.height = "223px"
+}
+
+function clickdblEvent(){
+    document.getElementById("demo2").innerHTML = "Double Clicked"
+    document.getElementById("demo2").style.background = "green"
+}
+
